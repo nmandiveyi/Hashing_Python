f79b718665a79f78617c5d65fd9413113cad7402
Ngonidzashe Mandiveyi <ngonidzashehh@gmail.com>
2020-10-23 19:58:55 -0700

    Created Python_file and README.md

---
+++ Python_file.py
@@ -0,0 +1,29 @@
+#############################################################################
+##                                                                         ##
+##                        Mini Programming Projects                        ##
+##                          Ngonidzashe Mandiveyi                          ##
+##                               Fall 2020                                 ##
+##                                                                         ##
+#############################################################################
+
+## This is is a program that generates a random number between 0 and n
+#import random
+#user_input = input("Roll the dice?")
+#n = int(input("Number of sides?"))
+
+#def roll_dice(user_input, n):
+#    if user_input == "yes":
+#        print(random.randrange(1,n,1))
+#    else:
+#        exit
+#
+#roll_dice(user_input, n);
+
+def factorial(n):
+    result = 1
+    for num in range(1, n+1):
+        result *= num
+
+    return result
+
+    
