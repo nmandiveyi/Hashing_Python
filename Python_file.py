e4461bfc7c6de64c39371c58bafc86ce28fb0053
Ngonidzashe Mandiveyi <ngonidzashehh@gmail.com>
2020-10-23 20:23:43 -0700

    Uncommented the factorial function

--- Python_file.py
+++ Python_file.py
@@ -7,17 +7,18 @@
 #############################################################################
 
 ## This is is a program that generates a random number between 0 and n
-#import random
-#user_input = input("Roll the dice?")
-#n = int(input("Number of sides?"))
 
-#def roll_dice(user_input, n):
-#    if user_input == "yes":
-#        print(random.randrange(1,n,1))
-#    else:
-#        exit
-#
-#roll_dice(user_input, n);
+import random
+user_input = input("Roll the dice?")
+n = int(input("Number of sides?"))
+
+def roll_dice(user_input, n):
+    if user_input == "yes":
+        print(random.randrange(1,n,1))
+    else:
+        exit
+
+roll_dice(user_input, n);
 
 def factorial(n):
     result = 1
